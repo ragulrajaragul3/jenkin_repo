95ee4992fd62f2de4aa060bf44d3475aabfb2412
ragulrajaragul3@gmail.com <136784755+ragulrajaragul3@users.noreply.github.com>
2024-03-28 16:28:24 +0530

    myfirtscommit

---
+++ jenkinpython.py
@@ -0,0 +1,11 @@
+import datetime
+
+def main():
+    # Get the current date
+    current_date = datetime.date.today()
+
+    # Print the current date
+    print("Today's date is:", current_date)
+
+if __name__ == "__main__":
+    main()
