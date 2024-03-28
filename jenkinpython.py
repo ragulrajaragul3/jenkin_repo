d0d90b61de8e3cb867f0781bfe2dad101a9ed97d
ragulrajaragul3@gmail.com <136784755+ragulrajaragul3@users.noreply.github.com>
2024-03-28 17:02:14 +0530

    update

--- jenkinpython.py
+++ jenkinpython.py
@@ -5,7 +5,7 @@ def main():
     current_date = datetime.date.today()
 
     # Print the current date
-    print("Today's date is:", current_date)
+    print("Today's current date is:", current_date)
 
 if __name__ == "__main__":
     main()
